edc2f498ffcdebaed1a458750a4ebde8275945d1
DenisRadu08 <denisradu04dr04@yahoo.com>
2026-04-14 18:50:20 +0300

    made some progress

--- agent.py
+++ agent.py
@@ -1,7 +1,12 @@
 import zmq
+import json
+import math
 
 class PlayerAgent:
     def __init__(self, agent_id):
+        self.x = agent_id * 10
+        self.y = 50
+        self.state = "IDLE"
         self.agent_id=agent_id
         self.context=zmq.Context()
         # Sockets
@@ -13,6 +18,8 @@ class PlayerAgent:
         self.poller = zmq.Poller()
         # vrem sa ascultam daca motorul a strigat unde e mingea
         self.poller.register(self.sub_socket, zmq.POLLIN)
+        # ii spunem poller-ului sa stea cu ochii si pe DEALER
+        self.poller.register(self.dealer_socket, zmq.POLLIN)
         # conectarea la motor
         self.sub_socket.connect("tcp://localhost:5555")
         self.dealer_socket.connect("tcp://localhost:5556")
@@ -25,10 +32,28 @@ class PlayerAgent:
             if self.sub_socket in events:
                 message = self.sub_socket.recv_json()
                 print(f"Agentul {self.agent_id} vede mingea la Coordonatele {message['x']}, {message['y']}")
-                # trimitem intentia la motor
-                intention = {
-                    "agent_id" : self.agent_id,
-                    "action" : "follow_ball"
-                }
-                self.dealer_socket.send_json(intention)
+                # daca agentul e aproape de minge, trimite intentia de a o urmari
+                # mealy machine
+                distance_from_ball = self.calculate_distance(message['x'],message['y'])
+                if distance_from_ball < 20 and self.state == "IDLE":
+                    intention = {
+                        "agent_id" : self.agent_id,
+                        "action" : "chase ball"
+                    }
+                    self.state = "CHASE"
+                    print(f"Agentul {self.agent_id} a intrat in starea CHASE")
+                    self.dealer_socket.send_json(intention)
+                elif self.state == "CHASE" and distance_from_ball>30:
+                    self.state = "IDLE"
+                    print(f"Agentul {self.agent_id} a intrat in starea IDLE")
+            # verificam daca motorul ne-a raspuns
+            if self.dealer_socket in events:
+                reply_bytes = self.dealer_socket.recv()
+                reply = json.loads(reply_bytes.decode('utf-8'))
+                print(f"Agentul {self.agent_id} a primit raspunsul: {reply['status']}")
         
+    # calculam distanta dintre agent si minge
+    def calculate_distance(self, ball_x, ball_y):
+        distance = math.sqrt((self.x-ball_x)**2 + (self.y - ball_y)**2)
+        return distance
+
--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 import zmq
 import time
 import json
+import math
 
 class MatchEngine:
     def __init__(self):
@@ -17,6 +18,8 @@ class MatchEngine:
         self.poller.register(self.router_socket, zmq.POLLIN)
         # Mingea
         self.ball = {"x": 50, "y": 50}
+        # Jucatori
+        self.players = {}
 
     
     def run(self):
@@ -28,8 +31,37 @@ class MatchEngine:
                 message = self.router_socket.recv_multipart()
                 agent_identity, agent_message = message
                 intention = json.loads(agent_message.decode('utf-8'))
+                # adaugam jucatorul in dictionar daca nu exista
+                if intention["agent_id"] not in self.players:
+                    self.players[intention["agent_id"]] = {"x": 10 + intention["agent_id"], "y": 10 * intention["agent_id"]}
                 print(f"Motorul valideaza actiunea '{intention['action']}' de la Agentul {intention['agent_id']}")
+                # daca suntem in chase ball, mergem spre minge
+                if intention["action"] == "chase ball":
+                    player_x = self.players[intention["agent_id"]]["x"]
+                    player_y = self.players[intention["agent_id"]]["y"]
 
+                    ball_x = self.ball["x"]
+                    ball_y = self.ball["y"]
+
+                    diff_x = ball_x - player_x
+                    diff_y = ball_y - player_y
+
+                    distance = math.hypot(diff_x, diff_y)
+
+                    if distance > 0:
+                        self.players[intention["agent_id"]]["x"] += diff_x / distance
+                        self.players[intention["agent_id"]]["y"] += diff_y / distance
+
+
+                    
+                reply_dict = {"status": "approved"}
+                reply_bytes = json.dumps(reply_dict).encode('utf-8')
+                self.router_socket.send_multipart([agent_identity, reply_bytes])
+            # miscarea mingii
+            if self.ball['x']<100 :
+                self.ball['x'] += 1
+            else:
+                self.ball['x'] = 0
             # broadcast la starea mingii
             self.pub_socket.send_json(self.ball)
 
